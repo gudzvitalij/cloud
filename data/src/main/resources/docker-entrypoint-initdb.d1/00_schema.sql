aaf5c68b956e930c863ce2d540cebf4fdf8361f9
Gudz Vitalij <enigmmm@yandex.ru>
2021-10-13 18:38:38 +0300

    fixed #3

--- data/src/main/resources/docker-entrypoint-initdb.d1/00_schema.sql
+++ data/src/main/resources/docker-entrypoint-initdb.d1/00_schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE payments
 (
     id       BIGSERIAL PRIMARY KEY,
-    "senderId" BIGINT NOT NULL,
+    "sender_Id" BIGINT NOT NULL,
     amount   BIGINT NOT NULL,
     comment  TEXT   NOT NULL,
     card_number VARCHAR (19) NOT NULL
